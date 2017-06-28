da21d56c474d6d44cdf82c704d855374c6d76718
yjmyzz <yjmyzz@126.com>
2017-06-28 23:33:28 +0800

    增加feign示例

--- service-consumer/src/main/java/com/cnblogs/yjmyzz/spring/cloud/study/service/client/UserFeignClient.java
+++ service-consumer/src/main/java/com/cnblogs/yjmyzz/spring/cloud/study/service/client/UserFeignClient.java
@@ -1,18 +1,16 @@
 package com.cnblogs.yjmyzz.spring.cloud.study.service.client;
 
-import com.cnblogs.yjmyzz.spring.cloud.study.api.UserService;
 import com.cnblogs.yjmyzz.spring.cloud.study.dto.UserDTO;
-import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import feign.Param;
+import feign.RequestLine;
 
 /**
  * Created by yangjunming on 2017/6/28.
  */
-@FeignClient(name = "service-provider-demo", configuration = BasicAuthConfiguration.class)
-public interface UserFeignClient extends UserService {
+//@FeignClient(name = "service-provider-demo")
+public interface UserFeignClient {
 
-    @GetMapping(value = "/user/{id}")
-    UserDTO findUser(@PathVariable("id") Integer userId);
+    @RequestLine("GET /user/{id}")
+    UserDTO findUser(@Param("id") Integer userId);
 
 }
--- service-consumer/src/main/java/com/cnblogs/yjmyzz/spring/cloud/study/service/controller/OrderController.java
+++ service-consumer/src/main/java/com/cnblogs/yjmyzz/spring/cloud/study/service/controller/OrderController.java
@@ -2,7 +2,13 @@ package com.cnblogs.yjmyzz.spring.cloud.study.service.controller;
 
 import com.cnblogs.yjmyzz.spring.cloud.study.dto.UserDTO;
 import com.cnblogs.yjmyzz.spring.cloud.study.service.client.UserFeignClient;
-import org.springframework.beans.factory.annotation.Autowired;
+import feign.Client;
+import feign.Feign;
+import feign.auth.BasicAuthRequestInterceptor;
+import feign.codec.Decoder;
+import feign.codec.Encoder;
+import org.springframework.cloud.netflix.feign.FeignClientsConfiguration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,24 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by yangjunming on 2017/6/17.
  */
+@Import(FeignClientsConfiguration.class)
 @RestController
 public class OrderController {
 
-    @Autowired
     private UserFeignClient userFeignClient;
 
-//    @Autowired
-//    private RestTemplate restTemplate;
-//
-//    @Autowired
-//    private LoadBalancerClient loadBalancerClient;
-//
-//    @Autowired
-//    private DiscoveryClient discoveryClient;
+    public OrderController(Decoder decoder, Encoder encoder, Client client) {
+        this.userFeignClient = Feign.builder().client(client)
+                .encoder(encoder)
+                .decoder(decoder)
+                .requestInterceptor(new BasicAuthRequestInterceptor("app01", "passwd01"))
+                .target(UserFeignClient.class, "http://service-provider-demo");
+    }
 
     @GetMapping("/order/{userId}/{orderNo}")
     public String findOrder(@PathVariable Integer userId, @PathVariable String orderNo) {
-        //UserDTO user = restTemplate.getForEntity("http://SERVICE-PROVIDER-DEMO/user/" + userId, UserDTO.class).getBody();
         UserDTO user = userFeignClient.findUser(userId);
         if (user != null) {
             return user.getUserName() + " 的订单" + orderNo + " 找到啦！";
@@ -35,16 +39,6 @@ public class OrderController {
 
         return "用户不存在！";
     }
-
-//    @GetMapping("/user-instance")
-//    public List<ServiceInstance> showInfo() {
-//        return this.discoveryClient.getInstances("SERVICE-PROVIDER-DEMO");
-//    }
-//
-//    @GetMapping("/log-instance")
-//    public ServiceInstance chooseInstance() {
-//        return this.loadBalancerClient.choose("SERVICE-PROVIDER-DEMO");
-//    }
 
 
 }
